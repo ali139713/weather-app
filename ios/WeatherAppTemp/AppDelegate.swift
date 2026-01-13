e94cdb97480f3646d4d16701ddaf54b45274e6de
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2026-01-14 02:16:57 +0500

    Fix app name to use valid iOS module name format

--- ios/WeatherAppTemp/AppDelegate.swift
+++ ios/WeatherAppTemp/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "weather-app",
+      withModuleName: "WeatherApp",
       in: window,
       launchOptions: launchOptions
     )
